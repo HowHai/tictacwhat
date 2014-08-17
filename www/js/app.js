f8c16dba8080d6ce294498554f521a86542572e0
Hai Nguyen <hmn91733@gmail.com>
2014-08-16 20:38:28 -0700

    Migrate old game code to controller

--- www/js/app.js
+++ www/js/app.js
@@ -18,5 +18,167 @@ angular.module('tictacwhat', ['ionic'])
   });
 })
 .controller('MainCtrl', function($scope) {
-  $scope.gameBoard = ['A','B','C','D','E','F','G','H','I'];
+  Array.prototype.last = function(){
+    return this[this.length -1];
+  }
+
+  var getInnerText = function(id) {
+    return document.getElementById(id).innerText;
+  }
+
+  function displayStatus(status){
+  };
+
+  var playerOneMoves = [];
+  var playerTwoMoves = [];
+  var gameBoard = [];
+  var botMoves = [];
+  var fatalBlow = [];
+  var gameOver = true;
+  var botMode = false;
+  var playerMode = false;
+
+  $scope.gameArray = [[0,1,2],[3,4,5],[6,7,8]];
+
+  var winCondition = [
+                          [0,1,2], [3,4,5], [6,7,8],
+                          [0,3,6], [1,4,7], [2,5,8],
+                          [0,4,8], [2,4,6]
+                     ];
+
+  $scope.botMode = function(){
+    gameOver = false;
+    botMode = true;
+    displayStatus("France's Turn");
+  };
+
+  // Return true if area is taken
+  function areaTaken(territory){
+    var getTerritory = getInnerText(territory);
+    return isNaN(getTerritory);
+  };
+
+  $scope.selectedTerritory = function(selected){
+    var getDiv = document.getElementById(selected);
+    var areaTaken = isNaN(getDiv.innerHTML);
+    var XorO = gameBoard.length % 2 == 0 ? "X" : "O";
+
+    function playerAction(player, turn){
+      player.push(selected);
+      gameBoard.push(selected);
+      getDiv.innerHTML = "<span>" + XorO + "</span>";
+      displayStatus(turn);
+    };
+
+    if (XorO == 'X' && !areaTaken && !gameOver)
+    {
+      playerAction(playerOneMoves, "Holland's Turn");
+      if (botMode)
+      {
+        // TODO: Require another click to show victory when timer used
+        setTimeout(function() { botAI(); }, 2000);
+        displayStatus("Holland is thinking...");
+      }
+    }
+    else if (!areaTaken && !gameOver && playerMode)
+      playerAction(playerTwoMoves, "France's Turn");
+
+    winCheck(winCondition, playerOneMoves, "France won!");
+    winCheck(winCondition, playerTwoMoves, "Holland won!");
+    winCheck(winCondition, botMoves, "Holland won!");
+  };
+
+  function winCheck(winCondition, playerMoves, message) {
+    for (var i = 0; i < winCondition.length; i++){
+      var winComb = winCondition[i].filter(function(value){
+        return playerMoves.indexOf(value) != -1;
+      })
+
+      if (winComb.length == 3)
+        {
+          displayStatus(message);
+          gameOver = true;
+          break;
+        }
+    }
+    if (playerMoves.length == 5 && gameOver == false)
+    {
+      displayStatus("War is hell... stalemate!");
+      gameOver = true;
+    }
+  };
+
+  // TODO: Remove all winCondition... do not need.
+  function botMoveChecker(winCondition, player, decision){
+    for (var i = 0; i < winCondition.length; i++)
+    {
+      var winningComb = winCondition[i].filter(function(value){
+        return player.indexOf(value) != -1;
+      })
+
+      var winningMove = winCondition[i].filter(function(value){
+        return player.indexOf(value) == -1;
+      })
+
+      if (winningComb.length == 2 && !areaTaken(winningMove))
+      {
+        return fatalBlow = winningMove;
+        break;
+      }
+
+      // Decision is true when player makes trap/odd moves
+      if (decision && winningComb.length == 1)
+      {
+        var moveOne = Math.abs(botMoves.last() - winningMove[0]);
+        var moveTwo = Math.abs(botMoves.last() - winningMove[1]);
+
+        var moveToTake = moveOne > moveTwo ? winningMove[0] : winningMove[1];
+        if (areaTaken(winningComb) && !areaTaken(winningMove[0]) && !areaTaken(winningMove[1]))
+        {
+          // Hacky solution to bot's only weakness
+          fatalBlow = (areaTaken(1) && areaTaken(8) && gameBoard.length == 5) ? 6 : moveToTake;
+          break;
+        }
+        else if (areaTaken(winningMove[0]) && areaTaken(winningMove[1]))
+          fatalBlow = areaTaken(3) ? 1 : 3
+      }
+    }
+  };
+
+  function calculateBotMove(player, bot){
+    // Make winning move if available
+    botMoveChecker(winCondition, bot);
+
+    // Bot reacts to player's move
+    if (fatalBlow.length == 0)
+      botMoveChecker(winCondition, player);
+  };
+
+  // AI MADNESS
+  function botAI(){
+    function pushData(data){
+      document.getElementById(data).innerHTML = "<span>O</span>";
+      botMoves.push(data);
+      gameBoard.push(data);
+      displayStatus("France's Turn");
+      fatalBlow = [];
+    }
+
+    var randMove = [0,2,6,8][Math.floor(Math.random() * [0,2,6,8].length)];
+
+    // Take random corner if center is taken on first move
+    if (gameBoard.length == 1)
+      areaTaken(4) ? pushData(randMove) : pushData(4);
+    else if (playerOneMoves.length < 5)
+    {
+      calculateBotMove(playerOneMoves, botMoves);
+      if (fatalBlow.length == 1 && !isNaN(fatalBlow))
+        pushData(fatalBlow[0]);
+      else
+      {
+        botMoveChecker(winCondition, botMoves, true);
+        pushData(fatalBlow);
+      }
+    }
+  };
 });
